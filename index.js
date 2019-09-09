2d5ce6eb29431da909e3a33fc3c10eae54a0ac87
generous-calculator-4715 <andrew.lewell@gmail.com>
2019-09-09 11:53:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,9 @@ function sayHiToGrandma(string) {
   
   if (string === lowercase) {
     return "I can't hear you!"
-  } else 
+  } else if (string === uppercase) {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma") {
+    return "I love you, too."
+  }
 }
